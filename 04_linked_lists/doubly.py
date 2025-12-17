30fbf280beeda3d96c838ea2f1044be90346c76a
Hithcgernn <179596180+Hitchgernn@users.noreply.github.com>
2025-12-17 20:15:48 +0700

    chore: add doubly linked list

--- 04_linked_lists/doubly.py
+++ 04_linked_lists/doubly.py
@@ -10,7 +10,10 @@ class DoublyLinkedList:
     
     def insert_front(self, data):
         new_node = Node(data)
-        new_node.next = self.head
+        if self.head is not None:
+            self.head.prev = new_node
+            new_node.next = self.head
+        
         self.head = new_node
 
     def insert_end(self, data):
@@ -24,3 +27,52 @@ class DoublyLinkedList:
             curr = curr.next
 
         curr.next = new_node
+        new_node.prev = curr
+
+    def delete(self, value):
+        curr = self.head
+
+        if curr is None:
+            print("list is empyt")
+            return
+        
+        if curr.data == value:
+            self.head = curr.next
+            if self.head is not None:
+                self.head.prev = None
+            return 
+        
+        while curr is not None and curr.data != value:
+            curr = curr.next
+        
+        if curr is None:
+            print("value not found")
+            return
+        
+        if curr.next is not None:
+            curr.next.prev = curr.prev
+        curr.prev.next = curr.next
+
+    def display(self):
+        print("list display")
+        print("None", end=" <-> ")
+
+        curr = self.head
+        while curr:
+            print(curr.data, end=" <-> ")
+            curr = curr.next
+
+        print("None")
+
+if __name__ == "__main__":
+    sll = DoublyLinkedList()
+
+    sll.insert_front(10)
+    sll.insert_front(20)
+    sll.insert_end(30)
+    sll.insert_end(40)
+
+    sll.display()
+    
+    sll.delete(30)
+    sll.display()
